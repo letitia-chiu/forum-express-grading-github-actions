1c13c6b67fa4f28a9a8b7a68af35779f2900efb6
letitia-chiu <hsuan.acc@outlook.com>
2023-12-08 11:35:13 +0800

    feat: add delete comment for admin

--- controllers/comment-controllers.js
+++ controllers/comment-controllers.js
@@ -12,14 +12,27 @@ const commentController = {
       Restaurant.findByPk(restaurantId)
     ])
       .then(([user, restaurant]) => {
-        if (!user) throw new Error("User didn't exist")
-        if (!restaurant) throw new Error("Restaurant didn't exist")
+        if (!user) throw new Error("User didn't exist!")
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
 
         return Comment.create({ text, restaurantId, userId })
       })
       .then(() => {
         req.flash('success_messages', '已新增評論！')
         res.redirect(`/restaurants/${restaurantId}`)
+      })
+      .catch(err => next(err))
+  },
+  deleteComment: (req, res, next) => {
+    return Comment.findByPk(req.params.id)
+      .then(comment => {
+        if (!comment) throw new Error("Comment didn't exist!")
+
+        return comment.destroy()
+      })
+      .then(deletedComment => {
+        req.flash('success_messages', '已刪除評論！')
+        res.redirect(`/restaurants/${deletedComment.restaurantId}`)
       })
       .catch(err => next(err))
   }

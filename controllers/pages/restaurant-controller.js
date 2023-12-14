7875236ff23c2acfc5efb06a180e31a5491f9b53
letitia-chiu <hsuan.acc@outlook.com>
2023-12-14 13:38:14 +0800

    feat: add restaurant-services

--- controllers/pages/restaurant-controller.js
+++ controllers/pages/restaurant-controller.js
@@ -1,135 +1,20 @@
-const { Restaurant, Category, Comment, User, Favorite } = require('../../models')
-const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+const restaurantServices = require('../../services/restaurant-services')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const DEFAULT_LIMIT = 9
-
-    const categoryId = Number(req.query.categoryId) || ''
-
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-
-    return Promise.all([
-      Restaurant.findAndCountAll({
-        where: {
-          ...categoryId ? { categoryId } : {}
-        },
-        limit,
-        offset,
-        raw: true,
-        nest: true,
-        include: [Category]
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        const data = restaurants.rows.map(r => ({
-          ...r,
-          description: r.description.substring(0, 50),
-          isFavorited: req.user && req.user.FavoritedRestaurants.some(fr => fr.id === r.id),
-          isLiked: req.user && req.user.LikedRestaurants.some(lr => lr.id === r.id)
-        }))
-        return res.render('restaurants', {
-          restaurants: data,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count)
-        })
-      })
-      .catch(err => next(err))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('restaurants', data))
   },
   getRestaurant: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id, {
-      include: [
-        Category,
-        { model: Comment, include: User }
-      ]
-    })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-
-        return restaurant.increment('viewCounts')
-      })
-      .then(restaurant => {
-        const isFavorited = req.user.FavoritedRestaurants.some(fr => fr.id === restaurant.id)
-        // 此處 isFavorite 處理方法與教案不同，教案採用 include User 資料，比對 restaurant.FavoritedUsers 是否包含登入之使用者。但考量到既然 passport 中已經夾帶 user.FavoritedRestaurants，直接比對此處資料是否包含當前餐廳 id，效率應該會更好，故改寫成以上形式。
-
-        const isLiked = req.user.LikedRestaurants.some(lr => lr.id === restaurant.id)
-
-        res.render('restaurant', {
-          restaurant: restaurant.toJSON(),
-          isFavorited,
-          isLiked
-        })
-      })
-      .catch(err => next(err))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('restaurant', data))
   },
   getDashboard: (req, res, next) => {
-    const restaurantId = req.params.id
-    return Promise.all([
-      Restaurant.findByPk(restaurantId, {
-        raw: true,
-        nest: true,
-        include: [Category]
-      }),
-      Comment.count({ where: { restaurantId } }),
-      Favorite.count({ where: { restaurantId } })
-    ])
-      .then(([restaurant, commentCounts, favoriteCounts]) => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-
-        res.render('dashboard', {
-          restaurant,
-          commentCounts,
-          favoriteCounts
-        })
-      })
-      .catch(err => next(err))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('dashboard', data))
   },
   getFeeds: (req, res, next) => {
-    return Promise.all([
-      Restaurant.findAll({
-        limit: 10,
-        order: [['createdAt', 'DESC']],
-        include: [Category],
-        raw: true,
-        nest: true
-      }),
-      Comment.findAll({
-        limit: 10,
-        order: [['createdAt', 'DESC']],
-        include: [User, Restaurant],
-        raw: true,
-        nest: true
-      })
-    ])
-      .then(([restaurants, comments]) => {
-        res.render('feeds', { restaurants, comments })
-      })
-      .catch(err => next(err))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('feeds', data))
   },
   getTopRestaurants: (req, res, next) => {
-    console.log('=========== Top 10 Restaurants ===========')
-    return Restaurant.findAll({
-      include: [
-        { model: User, as: 'FavoritedUsers' }
-      ]
-    })
-      .then(restaurants => {
-        const result = restaurants.map(r => ({
-          ...r.toJSON(),
-          description: r.description.length > 100 ? r.description.substring(0, 100) + '...' : r.description,
-          favoritedCount: r.FavoritedUsers.length,
-          isFavorited: req.user && req.user.FavoritedRestaurants.some(fr => fr.id === r.id)
-        }))
-          .sort((a, b) => b.favoritedCount - a.favoritedCount)
-          .slice(0, 10)
-
-        res.render('top-restaurants', { restaurants: result })
-      })
-      .catch(err => next(err))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('top-restaurants', data))
   }
 }
 module.exports = restaurantController
---
+++ services/restaurant-services.js
@@ -0,0 +1,133 @@
+const { Restaurant, Category, Comment, User, Favorite } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
+
+const restaurantServices = {
+  getRestaurants: (req, cb) => {
+    const DEFAULT_LIMIT = 9
+
+    const categoryId = Number(req.query.categoryId) || ''
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        limit,
+        offset,
+        raw: true,
+        nest: true,
+        include: [Category]
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.rows.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50),
+          isFavorited: req.user && req.user.FavoritedRestaurants.some(fr => fr.id === r.id),
+          isLiked: req.user && req.user.LikedRestaurants.some(lr => lr.id === r.id)
+        }))
+        return cb(null, {
+          restaurants: data,
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
+        })
+      })
+      .catch(err => cb(err))
+  },
+  getRestaurant: (req, cb) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        return restaurant.increment('viewCounts')
+      })
+      .then(restaurant => {
+        const isFavorited = req.user.FavoritedRestaurants.some(fr => fr.id === restaurant.id)
+        const isLiked = req.user.LikedRestaurants.some(lr => lr.id === restaurant.id)
+
+        return cb(null, {
+          restaurant: restaurant.toJSON(),
+          isFavorited,
+          isLiked
+        })
+      })
+      .catch(err => cb(err))
+  },
+  getDashboard: (req, cb) => {
+    const restaurantId = req.params.id
+    return Promise.all([
+      Restaurant.findByPk(restaurantId, {
+        raw: true,
+        nest: true,
+        include: [Category]
+      }),
+      Comment.count({ where: { restaurantId } }),
+      Favorite.count({ where: { restaurantId } })
+    ])
+      .then(([restaurant, commentCounts, favoriteCounts]) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        return cb(null, {
+          restaurant,
+          commentCounts,
+          favoriteCounts
+        })
+      })
+      .catch(err => cb(err))
+  },
+  getFeeds: (req, cb) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [Category],
+        raw: true,
+        nest: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        return cb(null, { restaurants, comments })
+      })
+      .catch(err => cb(err))
+  },
+  getTopRestaurants: (req, cb) => {
+    return Restaurant.findAll({
+      include: [
+        { model: User, as: 'FavoritedUsers' }
+      ]
+    })
+      .then(restaurants => {
+        const result = restaurants.map(r => ({
+          ...r.toJSON(),
+          description: r.description.length > 100 ? r.description.substring(0, 100) + '...' : r.description,
+          favoritedCount: r.FavoritedUsers.length,
+          isFavorited: req.user && req.user.FavoritedRestaurants.some(fr => fr.id === r.id)
+        }))
+          .sort((a, b) => b.favoritedCount - a.favoritedCount)
+          .slice(0, 10)
+
+        return cb(null, { restaurants: result })
+      })
+      .catch(err => cb(err))
+  }
+}
+
+module.exports = restaurantServices

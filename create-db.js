c03923c1eb0249276d4f231b65db2253475a30b6
letitia-chiu <hsuan.acc@outlook.com>
2023-12-27 17:34:18 +0800

    Publish: update README, add careate-db.js & .env.example

---
+++ create-db.js
@@ -0,0 +1,35 @@
+// *** User Settings *** //
+
+// Please modify "hostname", "user", "password" to match your MySQL server connection settings in /config/config.json
+// You can also change the "database" name if it already exists.
+
+const { development: UserSetting } = require('./config/config')
+
+const mysql = require('mysql2')
+
+// Create connection
+const connection = mysql.createConnection({
+  host: UserSetting.host,
+  user: UserSetting.username,
+  password: UserSetting.password
+})
+
+// Connect to MySQL server
+connection.connect(err => {
+  if (err) throw err
+  console.log('Connected to MySQL server.')
+
+  // Create database
+  connection.query('USE ' + UserSetting.database, (err, result) => {
+    if (err) {
+      connection.query(`CREATE DATABASE IF NOT EXISTS ${UserSetting.database}`, (err, result) => {
+        if (err) throw err
+        console.log('Database created.')
+        process.exit()
+      })
+    } else {
+      console.log('Database already exists.')
+      process.exit()
+    }
+  })
+})
